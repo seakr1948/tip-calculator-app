df90cd7dc114fe3672fb4c1f6f8f0d59d5a4acda
seakr1948 <seakr1948@gmail.com>
2021-11-30 18:44:25 -0800

    change input to have numeric keypad on mobile devices

--- src/components/InputPeople.js
+++ src/components/InputPeople.js
@@ -24,7 +24,7 @@ const InputPeople = ({Label, icon, setNumber, Number}) => {
                 <img src={icon} alt={Label} />
                 <input 
                     className="input" 
-                    type="number" 
+                    type="text" pattern="\d*"
                     min="0" 
                     placeholder="0" 
                     onChange={(event) => {setNumber(event.target.value)}} 
